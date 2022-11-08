0ff1229a97f76fcf0cf08feda0c08cbec82115bf
Zane Chan <dung@MacBook-Pro-Can.local>
2022-11-07 21:21:15 -0500

    Added unit tests for GET fragments/:id/info route, updated post.js file

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,24 +1,28 @@
 const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const logger = require('../../logger');
+const api = process.env.API_URL || 'http://localhost:8080';
 
 module.exports = async (req, res) => {
-  let fragment;
-  const api = process.env.API_URL;
-
   if (!Fragment.isSupportedType(req.get('Content-Type'))) {
     res.status(415).json(createErrorResponse(415, 'Content-Type is not supported'));
   } else {
     try {
-      fragment = new Fragment({
+      const fragment = new Fragment({
         ownerId: req.user,
-        type: 'text/plain', //req.get('Content-Type'),
+        type: req.get('Content-Type'), // 'text/plain',
         size: req.body.length,
       });
+      // save the fragment
       await fragment.save();
+      // save the binary data of the fragment
       await fragment.setData(req.body);
-      res.location(`${api}/v1/fragments/${fragment.id}`);
-      res.status(201).json(createSuccessResponse({ fragment }));
+
+      // set the location of the fragment and send back the response with the fragment data
+      res
+        .set('location', `${api}/v1/fragments/${fragment.id}`)
+        .status(201)
+        .send(createSuccessResponse({ fragment }));
       logger.info({ fragment: fragment }, `Fragment have been posted successfully`);
     } catch (err) {
       res.status(404).json(createErrorResponse(404, 'Unable to POST the fragment'));
--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -32,9 +32,8 @@ describe('GET /v1/fragments', () => {
       .send('This is a testing fragment')
       .set('Content-type', 'text/plain');
     const fragment = await readFragmentData(hash('user1@email.com'), req.body.fragment.id);
-    const id = req.body.fragment.id;
     const res = await request(app)
-      .get('/v1/fragments/' + id)
+      .get(`/v1/fragments/${req.body.fragment.id}`)
       .auth('user1@email.com', 'password1');
 
     expect(res.text).toBe(fragment.toString());
@@ -55,10 +54,8 @@ describe('GET /v1/fragments', () => {
       .send('<h2> Html </h2>')
       .set('Content-type', 'text/html');
 
-    const id = req.body.fragment.id;
-
     const res = await request(app)
-      .get('/v1/fragments/' + id + '.txt')
+      .get(`/v1/fragments/${req.body.fragment.id}.txt`)
       .auth('user1@email.com', 'password1');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('<h2> Html </h2>');
@@ -90,5 +87,34 @@ describe('GET /v1/fragments', () => {
       .get('/v1/fragments?expand=1')
       .auth('unauthenticated@email.com', 'password1');
     expect(res.statusCode).toBe(401);
+  });
+
+  test('Get fragments metadata by valid user ID', async () => {
+    const req = await request(app)
+      .post('/v1/fragments')
+      .send('This is a testing fragment')
+      .set('Content-Type', 'text/plain')
+      .auth('user1@email.com', 'password1');
+
+    const res = await request(app)
+      .get(`/v1/fragments/${req.body.fragment.id}/info`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(200);
+    expect(req.body).toEqual(res.body);
+  });
+
+  test('Get fragments metadata by invalid user ID', async () => {
+    const req = await request(app)
+      .post('/v1/fragments')
+      .send('This is a fragment')
+      .set('Content-Type', 'text/plain')
+      .auth('user1@email.com', 'password1');
+
+    const res = await request(app)
+      .get(`/v1/fragments/${req.body.fragment.id}abc/info`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(404);
   });
 });
