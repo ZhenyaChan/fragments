9c0407feb0799c62bb3a4a69b66eaf0ecbacd706
Zane Chan <dung@MacBook-Pro-Can.local>
2022-11-23 13:50:25 -0500

    Updated code to fix bugs based on provided feedback

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,4 +1,4 @@
-const { createSuccessResponse } = require('../../response');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 
@@ -6,12 +6,11 @@ const logger = require('../../logger');
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
-  let isTrue = false;
-  if (req.query.expand == 1) {
-    isTrue = true;
+  try {
+    const fragments = await Fragment.byUser(req.user, req.query.expand);
+    res.status(200).send(createSuccessResponse({ fragments }));
+    logger.info({ fragments }, `User's fragment list have been retrieved successfully`);
+  } catch (err) {
+    res.status(404).send(createErrorResponse(404, err));
   }
-
-  const fragments = await Fragment.byUser(req.user, isTrue);
-  res.status(200).send(createSuccessResponse({ fragments }));
-  logger.info({ fragments }, `User's fragment list have been retrieved successfully`);
 };
--- src/routes/api/get_metadata.js
+++ src/routes/api/get_metadata.js
@@ -1,19 +1,13 @@
 const { createErrorResponse, createSuccessResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
-const path = require('path');
 
 /**
  * Get metadata of the fragment by its passed id (GET /fragments/:id/info)
  */
 module.exports = async (req, res) => {
   try {
-    const convertExt = path.extname(req.params.id); // get the extension
-    const fragmentId = path.basename(req.params.id, convertExt); // get the fragment ID
-
-    if (fragmentId) {
-      const fragment = await Fragment.byId(req.user, req.params.id);
-      res.status(200).send(createSuccessResponse({ fragment: fragment }));
-    }
+    const fragment = await Fragment.byId(req.user, req.params.id);
+    res.status(200).send(createSuccessResponse({ fragment: fragment }));
   } catch (err) {
     res.status(404).send(createErrorResponse(404, err));
   }
