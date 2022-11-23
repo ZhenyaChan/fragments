223dba34325d09510062166db89337f0374bcd8d
Zane Chan <dung@MacBook-Pro-Can.local>
2022-11-23 18:53:50 -0500

    Added AWS data model to work with AWS S3 database

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,3 +1,3 @@
-// Choose the appropriate back-end data strategy (either AWS or memory database)
-
-module.exports = require('./memory');
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
