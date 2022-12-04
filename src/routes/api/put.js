10660defefe4e7dcd2b7335330c3c85230e094a2
Zane Chan <dung@MacBook-Pro-Can.local>
2022-12-03 21:55:04 -0500

    Added PUT /v1/fragments/:id route and unit tests for this route

---
+++ src/routes/api/put.js
@@ -0,0 +1,22 @@
+const { Fragment } = require('../../model/fragment');
+const { createErrorResponse, createSuccessResponse } = require('../../response');
+const logger = require('../../logger');
+const api = process.env.API_URL;
+
+module.exports = async (req, res) => {
+  try {
+    const fragment = await Fragment.byId(req.user, req.params.id);
+    if (req.get('Content-Type') != fragment.type) {
+      res
+        .status(400)
+        .send(createErrorResponse(400, 'Fragment type can not be changed after it is created'));
+    } else {
+      await fragment.setData(req.body);
+      res.location(`${api}/v1/fragments/${fragment.id}`);
+      res.status(200).send(createSuccessResponse({ fragment }));
+      logger.info({ fragment: fragment }, `Successfully updated fragment data!`);
+    }
+  } catch (err) {
+    res.status(404).send(createErrorResponse(404, 'Unknown fragment'));
+  }
+};
